0db306bbb32c15d23c27851563d672be0a8b526a
Victor Jordan <vjordan@fitec.org.br>
2018-02-22 22:51:17 -0300

    primeiro config

---
+++ app/js/js.js
@@ -0,0 +1,71 @@
+$("#owl-clients").owlCarousel({
+    autoplay: true,
+    autoplayHoverPause: true,
+    loop: true,
+    margin: 20,
+    responsiveClass: true,
+    nav: true,
+    loop: true,
+    navText: ["<svg class='icone icon-arrow-left'><use xlink:href='#icon-arrow-left'></use></svg>", "<svg class='icone icon-arrow-right'><use xlink:href='#icon-arrow-right'></use></svg>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        568: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    }
+});
+
+// Select all links with hashes
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .not('.carousel-control')
+    .click(function (event) {
+        $("#bs-example-navbar-collapse-1").removeClass('in');
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 100
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+
+
+    $(window).on("scroll", function() {
+    if($(window).scrollTop() > 150) {
+        $(".header").addClass("active");
+    } else {
+        $(".header").removeClass("active");
+    }
+});
